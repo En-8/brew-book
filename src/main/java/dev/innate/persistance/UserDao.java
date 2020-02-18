30c9b34728a1624359dc433866aa8ff52a556064
Nate Elliott <nelliott2@madisoncollege.edu>
2020-02-17 19:20:29 -0800

    implement getAllUsers

--- src/main/java/dev/innate/persistance/UserDao.java
+++ src/main/java/dev/innate/persistance/UserDao.java
@@ -1,14 +1,27 @@
 package dev.innate.persistance;
 
-
 import dev.innate.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class UserDao {
     private final Logger logger = LogManager.getLogger(this.getClass());
+    private SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
     public List<User> getAllUsers() {
-        
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createQuery(query).getResultList();
+
+        return users;
     }
 }
