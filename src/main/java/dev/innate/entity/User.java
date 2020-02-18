78dbddbe77a58ee5436f7cb22a873afd0be4c4e2
Nate Elliott <nelliott2@madisoncollege.edu>
2020-02-17 18:56:27 -0800

    set up User and User Dao skeleton for Hibernate

--- src/main/java/dev/innate/entity/User.java
+++ src/main/java/dev/innate/entity/User.java
@@ -1,11 +1,56 @@
-package dev.innate.model;
+package dev.innate.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity(name = "User")
+@Table(name = "user")
 public class User {
+    @Column(name = "username")
     String username;
+    @Column(name = "password")
     String password;
+    @Column(name = "email")
     String email;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name="native", strategy = "native")
+    private int id;
 
     public User() {
 
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/main/java/dev/innate/persistance/UserDao.java
+++ src/main/java/dev/innate/persistance/UserDao.java
@@ -1,4 +1,14 @@
 package dev.innate.persistance;
 
+
+import dev.innate.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class UserDao {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    public List<User> getAllUsers() {
+        
+    }
 }
