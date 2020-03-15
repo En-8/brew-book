7e5aed95ce74af9ca32af7cdeea9039db75964ae
Nate Elliott <nelliott2@madisoncollege.edu>
2020-03-15 18:55:15 -0500

    create CreateBrew controller

--- src/main/java/dev/innate/controller/CreateBrew.java
+++ src/main/java/dev/innate/controller/CreateBrew.java
@@ -1,4 +1,18 @@
 package dev.innate.controller;
 
-public class CreateBrew {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/createBrew"})
+public class CreateBrew extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("createBrew.jsp");
+        dispatcher.forward(request, response);
+    }
 }
